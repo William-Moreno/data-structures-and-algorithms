52ca56543e9d41ed785897e0a5961d06a60009c6
William-Moreno <intrcptr@comcast.net>
2020-12-07 06:02:24 -0800

    same issue with for loop...

--- code-challenges/challenges-05.test.js
+++ code-challenges/challenges-05.test.js
@@ -188,11 +188,11 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const removeEvenValues = (arr) => {
-  arr.forEach((number,idx) => {
+  for(let i = 0 ; i < arr.length ; i++){
     if(number % 2 === 0){
-      arr.splice(idx,1);
+      arr.splice(i,1);
     }
-  });
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -312,7 +312,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);
