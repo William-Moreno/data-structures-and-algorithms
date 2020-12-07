26e86cec2e1242ca7f7bbf5e08a9e0e447dce886
William-Moreno <intrcptr@comcast.net>
2020-12-07 07:10:51 -0800

    challenge 8 passed

--- code-challenges/challenges-05.test.js
+++ code-challenges/challenges-05.test.js
@@ -235,7 +235,11 @@ Write a function named totalSumCSV that, given a string of comma-separated value
 
 const totalSumCSV = (str) => {
   let total = 0;
-  // Solution code here...
+  const sumArray = str.split(',');
+  for(let i = 0 ; i < sumArray.length ; i++){
+    let numberConversion = Number(sumArray[i]);
+    total += numberConversion;
+  }
   return total;
 };
 
@@ -353,7 +357,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should add up the numbers contained within the string', () => {
     expect(totalSumCSV('1,4,5,7,2')).toStrictEqual(19);
     expect(totalSumCSV('147')).toStrictEqual(147);
