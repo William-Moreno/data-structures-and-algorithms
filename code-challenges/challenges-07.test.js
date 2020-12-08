e6b3257dec4b5b0d443196e2851c528304667f28
William-Moreno <intrcptr@comcast.net>
2020-12-08 13:44:37 -0800

    challenges 5 & 6 passed

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -83,7 +83,11 @@ For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
 const charCode = (arr) => {
-  // Solution code here...
+  const charCodeArray = arr.map((val) => {
+    const str = val.toString();
+    return str.charCodeAt();
+  });
+  return charCodeArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -97,7 +101,17 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 ------------------------------------------------------------------------------------------------ */
 
 const evenOdd = (arr) => {
-  // Solution code here...
+  const evenOrOdd = arr.map(val => {
+    if(typeof(val) !== 'number'){
+      return 'N/A';
+    }
+    if(val % 2 === 0){
+      return 'even';
+    } else {
+      return 'odd';
+    }
+  });
+  return evenOrOdd;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -244,14 +258,14 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
