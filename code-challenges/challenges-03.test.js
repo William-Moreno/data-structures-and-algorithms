9c14010d83ecb082a97e1e7bbd8313e98910bf08
William-Moreno <intrcptr@comcast.net>
2021-01-12 15:14:13 -0800

    challenge 11 passed

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
 // to learn more about the cheerio library and what it is doing, look at their documentation: https://www.npmjs.com/package/cheerio
-const cheerio = require("cheerio");
+const cheerio = require('cheerio');
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
@@ -17,7 +17,7 @@ let $ = createSnippetWithJQuery(`
 </ul>
 `);
 
-const changeAllClassNames = () => $("li").addClass("fruit");
+const changeAllClassNames = () => $('li').addClass('fruit');
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -162,9 +162,9 @@ function Person(firstName, lastName, age) {
 }
 
 const people = [
-  new Person("Wes", "Washington", 25),
-  new Person("Casey", "Codefellow", 38),
-  new Person("Stan", "Seattle", 67),
+  new Person('Wes', 'Washington', 25),
+  new Person('Casey', 'Codefellow', 38),
+  new Person('Stan', 'Seattle', 67),
 ];
 
 const sortPeople = (arr) => {
@@ -228,23 +228,23 @@ function Meeting(dayOfWeek, start, end) {
   this.end = end;
 }
 const meetings = [
-  new Meeting("Monday", "0900", "1000"),
-  new Meeting("Wednesday", "1300", "1500"),
-  new Meeting("Tuesday", "1145", "1315"),
-  new Meeting("Wednesday", "0930", "1000"),
-  new Meeting("Monday", "0900", "0945"),
-  new Meeting("Friday", "1200", "1345"),
+  new Meeting('Monday', '0900', '1000'),
+  new Meeting('Wednesday', '1300', '1500'),
+  new Meeting('Tuesday', '1145', '1315'),
+  new Meeting('Wednesday', '0930', '1000'),
+  new Meeting('Monday', '0900', '0945'),
+  new Meeting('Friday', '1200', '1345'),
 ];
 
 const sortMeetingsByDay = (arr) => {
   arr.forEach((obj) => {
-    if (obj.dayOfWeek === "Monday") {
+    if (obj.dayOfWeek === 'Monday') {
       obj.weekDay = 1;
-    } else if (obj.dayOfWeek === "Tuesday") {
+    } else if (obj.dayOfWeek === 'Tuesday') {
       obj.weekDay = 2;
-    } else if (obj.dayOfWeek === "Wednesday") {
+    } else if (obj.dayOfWeek === 'Wednesday') {
       obj.weekDay = 3;
-    } else if (obj.dayOfWeek === "Thursday") {
+    } else if (obj.dayOfWeek === 'Thursday') {
       obj.weekDay = 4;
     } else {
       obj.weekDay = 5;
@@ -276,7 +276,52 @@ You DO NOT need to use your solution to Challenge 9 in completing Challenge 10.
 ------------------------------------------------------------------------------------------------ */
 
 const sortSchedule = (arr) => {
-  // Solution code here...
+  arr.forEach((obj) => {
+    if (obj.dayOfWeek === 'Monday') {
+      obj.weekDay = 1;
+    } else if (obj.dayOfWeek === 'Tuesday') {
+      obj.weekDay = 2;
+    } else if (obj.dayOfWeek === 'Wednesday') {
+      obj.weekDay = 3;
+    } else if (obj.dayOfWeek === 'Thursday') {
+      obj.weekDay = 4;
+    } else {
+      obj.weekDay = 5;
+    }
+  });
+
+  arr.sort((a, b) => {
+    if (a.end < b.end) {
+      return -1;
+    } else if (a.end > b.end) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  arr.sort((a, b) => {
+    if (a.start < b.start) {
+      return -1;
+    } else if (a.start > b.start) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  arr.sort((a, b) => {
+    if (a.weekDay < b.weekDay) {
+      return -1;
+    } else if (a.weekDay > b.weekDay) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  arr.forEach((obj) => delete obj.weekDay);
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -306,17 +351,17 @@ DO NOT CHANGE any of the below code.
 Run your tests from the console: jest challenges-03.test.js
 ------------------------------------------------------------------------------------------------ */
 
-describe("Testing challenge 1", () => {
-  test("It should add a class of fruit to all the list items", () => {
+describe('Testing challenge 1', () => {
+  test('It should add a class of fruit to all the list items', () => {
     changeAllClassNames();
 
-    expect($("li.apple").hasClass("fruit")).toBe(true);
-    expect($("li.orange").hasClass("fruit")).toBe(true);
+    expect($('li.apple').hasClass('fruit')).toBe(true);
+    expect($('li.orange').hasClass('fruit')).toBe(true);
   });
 });
 
-describe("Testing challenge 2", () => {
-  test("It should sort high-to-low the numbers in an array", () => {
+describe('Testing challenge 2', () => {
+  test('It should sort high-to-low the numbers in an array', () => {
     const nums = [3, 4, 5, 6, 7];
     expect(sortBackwards(nums)).toStrictEqual([7, 6, 5, 4, 3]);
     expect(sortBackwards([3, 2, 1])).toStrictEqual([3, 2, 1]);
@@ -326,58 +371,58 @@ describe("Testing challenge 2", () => {
   });
 });
 
-describe("Testing challenge 3", () => {
-  test("It should sort strings alphabetically", () => {
+describe('Testing challenge 3', () => {
+  test('It should sort strings alphabetically', () => {
     expect(
-      alphabetize(["alphabet", "Zebra", "Alphabet", "carrot"])
-    ).toStrictEqual(["Alphabet", "Zebra", "alphabet", "carrot"]);
-    expect(alphabetize(["alphabet", "Alphabet", "carrot"])).toStrictEqual([
-      "Alphabet",
-      "alphabet",
-      "carrot",
+      alphabetize(['alphabet', 'Zebra', 'Alphabet', 'carrot'])
+    ).toStrictEqual(['Alphabet', 'Zebra', 'alphabet', 'carrot']);
+    expect(alphabetize(['alphabet', 'Alphabet', 'carrot'])).toStrictEqual([
+      'Alphabet',
+      'alphabet',
+      'carrot',
     ]);
     expect(alphabetize([])).toStrictEqual([]);
   });
 });
 
-describe("Testing challenge 4", () => {
-  test("It should sort strings by length", () => {
-    const ans = sortByLength(["alphabet", "Zebra", "Alphabet", "carrot"]);
-    expect(ans.slice(0, 2)).toStrictEqual(["Zebra", "carrot"]);
+describe('Testing challenge 4', () => {
+  test('It should sort strings by length', () => {
+    const ans = sortByLength(['alphabet', 'Zebra', 'Alphabet', 'carrot']);
+    expect(ans.slice(0, 2)).toStrictEqual(['Zebra', 'carrot']);
     expect(ans.slice(2, 4)).toEqual(
-      expect.arrayContaining(["Alphabet", "alphabet"])
+      expect.arrayContaining(['Alphabet', 'alphabet'])
     );
-    expect(sortByLength(["a", "bc", ""])).toStrictEqual(["", "a", "bc"]);
-    expect(sortByLength(["a"])).toStrictEqual(["a"]);
+    expect(sortByLength(['a', 'bc', ''])).toStrictEqual(['', 'a', 'bc']);
+    expect(sortByLength(['a'])).toStrictEqual(['a']);
     expect(sortByLength([])).toStrictEqual([]);
   });
 });
 
-describe("Testing challenge 5", () => {
-  test("It should alphabetize without regard to capitalization", () => {
+describe('Testing challenge 5', () => {
+  test('It should alphabetize without regard to capitalization', () => {
     expect(
-      alphabetizeBetter(["Alice", "apple", "alert", "Average"])
-    ).toStrictEqual(["alert", "Alice", "apple", "Average"]);
-    const ans = alphabetizeBetter(["alphabet", "Zebra", "Alphabet", "carrot"]);
+      alphabetizeBetter(['Alice', 'apple', 'alert', 'Average'])
+    ).toStrictEqual(['alert', 'Alice', 'apple', 'Average']);
+    const ans = alphabetizeBetter(['alphabet', 'Zebra', 'Alphabet', 'carrot']);
     expect(ans.slice(0, 2)).toEqual(
-      expect.arrayContaining(["Alphabet", "alphabet"])
+      expect.arrayContaining(['Alphabet', 'alphabet'])
     );
-    expect(ans.slice(2)).toStrictEqual(["carrot", "Zebra"]);
+    expect(ans.slice(2)).toStrictEqual(['carrot', 'Zebra']);
   });
 });
 
-describe("Testing challenge 6", () => {
-  test("It should sort items by their price", () => {
+describe('Testing challenge 6', () => {
+  test('It should sort items by their price', () => {
     expect(
       sortByPrice([
-        { name: "Sweatshirt", price: 45 },
-        { name: "Bookmark", price: 2.5 },
-        { name: "Tote bag", price: 15 },
+        { name: 'Sweatshirt', price: 45 },
+        { name: 'Bookmark', price: 2.5 },
+        { name: 'Tote bag', price: 15 },
       ])
     ).toStrictEqual([
-      { name: "Bookmark", price: 2.5 },
-      { name: "Tote bag", price: 15 },
-      { name: "Sweatshirt", price: 45 },
+      { name: 'Bookmark', price: 2.5 },
+      { name: 'Tote bag', price: 15 },
+      { name: 'Sweatshirt', price: 45 },
     ]);
     expect(sortByPrice([{ price: 12 }, { price: 10 }])).toStrictEqual([
       { price: 10 },
@@ -387,8 +432,8 @@ describe("Testing challenge 6", () => {
   });
 });
 
-describe("Testing challenge 7", () => {
-  test("It should sort numbers by their length", () => {
+describe('Testing challenge 7', () => {
+  test('It should sort numbers by their length', () => {
     expect(sortNumbersByLength([10, 2.8, 1, -47.75])).toStrictEqual([
       1,
       10,
@@ -407,88 +452,88 @@ describe("Testing challenge 7", () => {
   });
 });
 
-describe("Testing challenge 8", () => {
-  test("It should sort people by their last names", () => {
+describe('Testing challenge 8', () => {
+  test('It should sort people by their last names', () => {
     expect(sortPeople(people)).toStrictEqual([
-      new Person("Casey", "Codefellow", 38),
-      new Person("Stan", "Seattle", 67),
-      new Person("Wes", "Washington", 25),
+      new Person('Casey', 'Codefellow', 38),
+      new Person('Stan', 'Seattle', 67),
+      new Person('Wes', 'Washington', 25),
     ]);
     expect(
-      sortPeople([{ lastName: "banana" }, { lastName: "apple" }])
-    ).toStrictEqual([{ lastName: "apple" }, { lastName: "banana" }]);
+      sortPeople([{ lastName: 'banana' }, { lastName: 'apple' }])
+    ).toStrictEqual([{ lastName: 'apple' }, { lastName: 'banana' }]);
   });
 });
 
-describe("Testing challenge 9", () => {
-  test("It should sort people with more strict ordering", () => {
+describe('Testing challenge 9', () => {
+  test('It should sort people with more strict ordering', () => {
     const family = [
-      new Person("Casey", "Codefellows", 55),
-      new Person("Casey", "Codefellows", 37),
-      new Person("Charlie", "Codefellows", 21),
-      new Person("Charles", "Codefellows", 29),
-      new Person("Carol", "Codefellow", 88),
+      new Person('Casey', 'Codefellows', 55),
+      new Person('Casey', 'Codefellows', 37),
+      new Person('Charlie', 'Codefellows', 21),
+      new Person('Charles', 'Codefellows', 29),
+      new Person('Carol', 'Codefellow', 88),
     ];
     expect(sortPeopleBetter(family)).toStrictEqual([
-      new Person("Carol", "Codefellow", 88),
-      new Person("Casey", "Codefellows", 37),
-      new Person("Casey", "Codefellows", 55),
-      new Person("Charles", "Codefellows", 29),
-      new Person("Charlie", "Codefellows", 21),
+      new Person('Carol', 'Codefellow', 88),
+      new Person('Casey', 'Codefellows', 37),
+      new Person('Casey', 'Codefellows', 55),
+      new Person('Charles', 'Codefellows', 29),
+      new Person('Charlie', 'Codefellows', 21),
     ]);
     expect(
       sortPeopleBetter([
-        { firstName: "andrew", lastName: "apple" },
-        { firstName: "andre", lastName: "apple" },
+        { firstName: 'andrew', lastName: 'apple' },
+        { firstName: 'andre', lastName: 'apple' },
       ])
     ).toStrictEqual([
-      { firstName: "andre", lastName: "apple" },
-      { firstName: "andrew", lastName: "apple" },
+      { firstName: 'andre', lastName: 'apple' },
+      { firstName: 'andrew', lastName: 'apple' },
     ]);
   });
 });
 
-describe("Testing challenge 10", () => {
-  test("It should sort meetings by the day on which they happen", () => {
+describe('Testing challenge 10', () => {
+  test('It should sort meetings by the day on which they happen', () => {
     const sortedMeetings = sortMeetingsByDay(meetings);
     expect(sortedMeetings.slice(0, 2)).toEqual(
       expect.arrayContaining([
-        new Meeting("Monday", "0900", "0945"),
-        new Meeting("Monday", "0900", "1000"),
+        new Meeting('Monday', '0900', '0945'),
+        new Meeting('Monday', '0900', '1000'),
       ])
     );
     expect(sortedMeetings[2]).toStrictEqual(
-      new Meeting("Tuesday", "1145", "1315")
+      new Meeting('Tuesday', '1145', '1315')
     );
     expect(sortedMeetings.slice(3, 5)).toEqual(
       expect.arrayContaining([
-        new Meeting("Wednesday", "0930", "1000"),
-        new Meeting("Wednesday", "1300", "1500"),
+        new Meeting('Wednesday', '0930', '1000'),
+        new Meeting('Wednesday', '1300', '1500'),
       ])
     );
     expect(sortedMeetings[5]).toStrictEqual(
-      new Meeting("Friday", "1200", "1345")
+      new Meeting('Friday', '1200', '1345')
     );
   });
 });
 
-xdescribe("Testing challenge 11", () => {
-  test("It should sort meetings by when they happen", () => {
+describe('Testing challenge 11', () => {
+  test('It should sort meetings by when they happen', () => {
     expect(sortSchedule(meetings)).toStrictEqual([
-      new Meeting("Monday", "0900", "0945"),
-      new Meeting("Monday", "0900", "1000"),
-      new Meeting("Tuesday", "1145", "1315"),
-      new Meeting("Wednesday", "0930", "1000"),
-      new Meeting("Wednesday", "1300", "1500"),
-      new Meeting("Friday", "1200", "1345"),
+      new Meeting('Monday', '0900', '0945'),
+      new Meeting('Monday', '0900', '1000'),
+      new Meeting('Tuesday', '1145', '1315'),
+      new Meeting('Wednesday', '0930', '1000'),
+      new Meeting('Wednesday', '1300', '1500'),
+      new Meeting('Friday', '1200', '1345'),
     ]);
   });
 });
 
-xdescribe("Testing challenge 12", () => {
-  test("It should add a class of pear to the thrid li", () => {
+xdescribe('Testing challenge 12', () => {
+  test('It should add a class of pear to the thrid li', () => {
     addPearClass();
-    expect($("li:nth-child(3)").hasClass("pear")).toBe(true);
+    expect($('li:nth-child(3)').hasClass('pear')).toBe(true);
   });
 });
 
