f052f220cb0849514b123f63622fea918f038c4c
William-Moreno <intrcptr@comcast.net>
2021-01-19 06:28:32 -0800

    challenge 8 passed

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -128,7 +128,15 @@ Write a function named findEvery that takes in an array of strings, along with a
 ------------------------------------------------------------------------------------------------ */
 
 const findEvery = (arr, target) => {
-  // Solution code here...
+  const inEvery = arr.reduce((accum, str) => {
+    if(accum === false || !str.includes(target)){
+      accum = false;
+    } else {
+      accum = true;
+    }
+    return accum;
+  }, true);
+  return inEvery;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -263,7 +271,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should determine whether all the strings contain a given string', () => {
     const words = ['things', 'apple pie (:)', ':)banana pie', 'missing that thing', 'cant:)aloupe is tasty'];
 
