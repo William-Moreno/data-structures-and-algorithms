7b4f8a474a0c6a47348c59d6718918430346c97f
William-Moreno <intrcptr@comcast.net>
2021-02-22 13:07:25 -0800

    append and insertBefore methods written

--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -47,7 +47,53 @@ class LinkedList {
     listString += `NULL`;
     return listString;
   }
+
+  append(value) {
+    let appendedNode = new Node(value);
+    let current = this.head;
+
+    while(current.next !== null) {
+      current = current.next;
+    }
+    current.next = appendedNode;
+  }
+
+  insertBefore(value, newVal) {
+    if(value === this.head.value) {
+      this.insert(newVal);
+      return this.head;
+    }
+
+    let newValNode = new Node(newVal);
+    let current = this.head;
+
+    while(current.next !== null) {
+      if(current.next.value === value) {
+        newValNode.next = current.next;
+        current.next = newValNode;
+        return this.head;
+      }
+      current = current.next;
+    }
+    return console.log('Exception');
+  }
+
+  insertAfter(value, newVal) {
+
+  }
 }
+
+const testList = new LinkedList();
+testList.head = new Node(0);
+testList.head.next = new Node(1);
+testList.head.next.next = new Node(2);
+testList.head.next.next.next = new Node(3);
+
+console.log(testList.toString());
+testList.append(4);
+console.log(testList.toString());
+testList.insertBefore(0, 5);
+console.log(testList.toString());
 
 
 module.exports = {
