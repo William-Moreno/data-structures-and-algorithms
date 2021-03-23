90525dda636150911110c84a1ec3779d58de353c
William-Moreno <intrcptr@comcast.net>
2021-03-23 13:44:00 -0700

    code written

---
+++ mergeSort/merge-sort.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const mergeSort = (arr) => {
+  console.log('splitting ', arr);
+  let n = arr.length;
+
+  if(n > 1) {
+    let mid = (n/2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+
+    mergeSort(left);
+
+    mergeSort(right);
+
+    merge(left, right, arr);
+  }
+
+  
+  return arr;
+
+};
+
+const merge = (left, right, arr) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while(i < left.length && j < right.length) {
+    if(left[i] < right[j]) {
+      arr[k] = left[i];
+      i++;
+    } else {
+      arr[k] = right[j];
+      j++;
+    }
+
+    k++;
+  }
+
+  while(i < left.length){
+    arr[k] = left[i];
+    i++;
+    k++;
+  }
+
+  while(j < right.length){
+    arr[k] = right[j];
+    j++;
+    k++;
+  }
+  console.log('Merging', arr);
+
+};
+
+
+let test = [8, 4, 23, 42, 16, 15];
+
+const sorted = mergeSort(test);
+
+console.log(sorted);
+
+
+module.exports = {
+  mergeSort: mergeSort,
+  merge: merge,
+};
