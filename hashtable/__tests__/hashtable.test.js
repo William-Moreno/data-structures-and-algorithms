24b03e07513b77483650bc87f4f03f407c8da453
William-Moreno <intrcptr@comcast.net>
2021-03-26 20:52:28 -0700

    All 6 tests written and passing

--- hashtable/__tests__/hashtable.test.js
+++ hashtable/__tests__/hashtable.test.js
@@ -7,6 +7,8 @@ let table = new HashTable(1024);
 table.add('Laurel', 'Hot Wife' );
 table.add('Riley', 'PlayStation Jockey' );
 table.add('Alexis', 'Dancer' );
+table.add('Jogo', 'Collision Target');
+
 
 describe('Testing hash table behavior', () => {
   it('Should take in a key / value pair and successfully insert it into the hashtable using add()', () => {
@@ -30,7 +32,6 @@ describe('Testing hash table behavior', () => {
 
   it('Should hash a key to an in-range value using hash()', () => {
     let result = table.hash('Doug');
-    console.log(result);
     if(result >= 0 && result < 1024){
       result = true;
     } else {
@@ -40,11 +41,17 @@ describe('Testing hash table behavior', () => {
     expect(result).toEqual(true);
   });
 
-  it('Should handle a collision within a hashtable when using add()', () => {
+  it('Should handle a collision within a hashtable by using a LinkedList in the bucket when using add()', () => {
     table.add('Doug', 'Test Dummy');
     let bucketValue = table.buckets[409];
-    console.log(bucketValue);
 
-    expect(bucketValue).toEqual(`{"head": {"next": null, "value": {"Doug": "Test Dummy"}}}`);
+    expect(bucketValue.head).toBeTruthy();
   });
+
+  it('Should successfully retreive a value from a bucket in the hashtable that has a collision with get()', () => {
+    let result = table.get('Jogo');
+
+    expect(result).toEqual('Collision Target');
+  });
+
 });
--- hashtable/hashtable.js
+++ hashtable/hashtable.js
@@ -41,7 +41,7 @@ class LinkedList {
       }
       current = current.next;
     }
-    return false;
+    return null;
   }
 }
 
